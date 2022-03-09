f1273e9b1c30e1421028e0767ff46804323d8784
public-jun <generalpublicjunjun@gmail.com>
2022-03-09 17:15:47 +0900

    Add(ft::equal): equal(InputIt1, InputIt1, InputIt2, BinaryPredicate)
    `BinaryPredicate` で イテレータが等値かどうか調べる
    
    About: #88
    See Also: #31

--- includes/utils/algorithm.hpp
+++ includes/utils/algorithm.hpp
@@ -18,6 +18,19 @@ bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
     }
     return true;
 }
+
+template <class InputIt1, class InputIt2, class BinaryPredicate>
+bool equal(InputIt1 first1, InputIt1 last2, InputIt2 first2, BinaryPredicate p)
+{
+    for (; first1 != last2; ++first1, ++first2)
+    {
+        if (!p(*first1, *first2))
+        {
+            return false;
+        }
+    }
+    return true;
+}
 } // namespace ft
 
 #endif
--- testfiles/test_algorithm.cpp
+++ testfiles/test_algorithm.cpp
@@ -5,9 +5,27 @@ template <class Iterator1, class Iterator2>
 void judge_equal(Iterator1 first1, Iterator1 last1, Iterator2 first2)
 {
     if (ft::equal(first1, last1, first2) == true)
-        cout << "EQUAL" <<endl;
+        cout << "EQUAL" << endl;
     else
-        cout << "NOT EQUAL" <<endl;
+        cout << "NOT EQUAL" << endl;
+}
+
+template <class Iterator1, class Iterator2, class BinaryPredicate>
+void judge_equal(Iterator1 first1, Iterator1 last1, Iterator2 first2,
+                 BinaryPredicate pred)
+{
+    // cout << "CALLED" << endl;
+    if (ft::equal(first1, last1, first2, pred) == true)
+        cout << "EQUAL" << endl;
+    else
+        cout << "NOT EQUAL" << endl;
+}
+
+bool func(int lhs, int rhs)
+{
+    if (lhs == rhs)
+        return true;
+    return false;
 }
 
 void equal_test()
@@ -42,7 +60,6 @@ void equal_test()
         judge_equal(v1.begin(), v1.end(), v2.begin());
     }
 
-
     {
         pout("same range, not equal value");
         std::vector<int> v1;
@@ -70,9 +87,62 @@ void equal_test()
 
         judge_equal(v1.begin(), v1.end(), v2.begin());
     }
+
+    {
+        pout("same range, equal value");
+        std::vector<int> v1;
+        for (int i = 0; i <= 5; ++i)
+            v1.push_back(i);
+        vdebug(v1);
+        std::vector<int> v2;
+        for (int i = 0; i <= 5; ++i)
+            v2.push_back(i);
+        vdebug(v2);
+
+        judge_equal(v1.begin(), v1.end(), v2.begin(), func);
+    }
+
+    {
+        pout("same range, equal value");
+        std::vector<int> v1;
+        for (int i = 0; i <= 5; ++i)
+            v1.push_back(i);
+        vdebug(v1);
+        std::vector<int> v2;
+        for (int i = 0; i <= 10; ++i)
+            v2.push_back(i);
+        vdebug(v2);
+
+        judge_equal(v1.begin(), v1.end(), v2.begin(), func);
+    }
+
+    {
+        pout("same range, not equal value");
+        std::vector<int> v1;
+        for (int i = 0; i <= 5; ++i)
+            v1.push_back(i);
+        vdebug(v1);
+        std::vector<int> v2;
+        for (int i = -1; i <= 4; ++i)
+            v2.push_back(i);
+        vdebug(v2);
+
+        judge_equal(v1.begin(), v1.end(), v2.begin(), func);
+    }
+
+    {
+        pout("diff range, not equal value");
+        std::vector<int> v1;
+        for (int i = 0; i <= 5; ++i)
+            v1.push_back(i);
+        vdebug(v1);
+        std::vector<int> v2;
+        for (int i = -1; i <= 20; ++i)
+            v2.push_back(i);
+        vdebug(v2);
+
+        judge_equal(v1.begin(), v1.end(), v2.begin(), func);
+    }
 }
 
-void algorithm_test()
-{
-    equal_test();
-}
+void algorithm_test() { equal_test(); }

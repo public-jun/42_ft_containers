4509abb066cc368e7316b7e780472bd640690fe5
public-jun <generalpublicjunjun@gmail.com>
2022-03-12 00:00:57 +0900

    Add(stack container): stack
    - Member type
    - Member func
    - Member var
    - Non member func
    
    About: #55
    See Also: #100 102

--- gtest/stack_test.cpp
+++ gtest/stack_test.cpp
@@ -3,8 +3,37 @@
 #include <stack>
 #include <gtest/gtest.h>
 
+namespace {
 
-TEST(Stack, Constructor)
+TEST(Stack, ConstructAndBasicOperation)
 {
-    std::stack<int> s;
+    ft::stack<int> int_stack;
+
+    int_stack.push(1);
+    int_stack.push(2);
+
+    EXPECT_EQ(int_stack.size(), 2ul);
+    EXPECT_EQ(int_stack.top(), 2);
+    int_stack.pop();
+    EXPECT_EQ(int_stack.size(), 1ul);
+    EXPECT_EQ(int_stack.top(), 1);
+    int_stack.pop();
+    EXPECT_EQ(int_stack.size(), 0ul);
+    EXPECT_TRUE(int_stack.empty());
 }
+
+TEST(Stack, CopyConstruct)
+{
+    ft::stack<int> int_stack1;
+
+    int_stack1.push(1);
+    int_stack1.push(2);
+
+    ft::stack<int> int_stack2 = int_stack1;
+    EXPECT_EQ(int_stack1, int_stack2);
+    int_stack2.push(1);
+    int_stack2.push(2);
+    EXPECT_NE(int_stack1, int_stack2);
+}
+
+} // namespace
--- includes/containers/stack.hpp
+++ includes/containers/stack.hpp
@@ -8,6 +8,7 @@ template <class Tp, class Container = ft::vector<Tp> >
 class stack
 {
 public:
+    // Member type
     typedef Container container_type;
     typedef typename container_type::value_type value_type;
     typedef typename container_type::reference reference;
@@ -15,8 +16,73 @@ public:
     typedef typename container_type::size_type size_type;
 
 protected:
-    container_type c;
+    container_type c_;
+
+public:
+    // Member function
+    explicit stack(const container_type& cont = Container()) : c_(cont) {}
+    stack(const stack& other) : c_(other.c_) {}
+    ~stack() {}
+    stack& operator=(const stack& other)
+    {
+        c_ = other.c_;
+        return *this;
+    }
+
+    reference top() { return c_.back(); }
+    const_reference top() const { return c_.back(); }
+
+    bool empty() const { return c_.empty(); }
+
+    size_type size() const { return c_.size(); }
+
+    void push(const value_type& value) { c_.push_back(value); }
+
+    void pop() { c_.pop_back(); }
+
+    template <class T1, class C1>
+    friend bool operator==(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs);
+
+    template <class T1, class C1>
+    friend bool operator<(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs);
 };
+
+// Non mamber function
+template <class T1, class C1>
+bool operator==(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs)
+{
+    return lhs.c_ == rhs.c_;
+}
+
+template <class T1, class C1>
+bool operator!=(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs)
+{
+    return !(lhs == rhs);
+}
+
+template <class T1, class C1>
+bool operator<(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs)
+{
+    return lhs.c_ < rhs.c_;
+}
+
+template <class T1, class C1>
+bool operator<=(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs)
+{
+    return !(lhs > rhs);
+}
+
+template <class T1, class C1>
+bool operator>(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs)
+{
+    return rhs < lhs;
+}
+
+template <class T1, class C1>
+bool operator>=(const stack<T1, C1>& lhs, const stack<T1, C1>& rhs)
+{
+    return !(lhs < rhs);
+}
 } // namespace ft
 
 #endif // STACK_HPP
